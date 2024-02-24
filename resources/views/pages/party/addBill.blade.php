6c2d7c839a8b6c9c51e7dc802a5d8113ad7114e8
D-Abdullah <eng.abdullah.12418@gmail.com>
2024-02-24 02:20:58 +0200

    bill

--- resources/views/pages/party/addBill.blade.php
+++ resources/views/pages/party/addBill.blade.php
@@ -7,11 +7,9 @@
 @endsection
 
 @section('content')
-    <!-- end of frame-5 (header) -->
-
-    <h2 class="mb-2 mt-5">اضافه حفله جديده</h2>
     <!-- start of body -->
-    <h2 class="mb-2 mt-5">اضافه معلومات الفاتوره</h2>
+    {{ $party->id }}
+    <h2 class="mb-2 mt-5">اضافه معلومات الفاتوره للحفله "{{ $party->name }}"</h2>
 
     <div class="mt-5 mb-5 special">
         <button class="secound-btn active-btn total-product mb-2 tabs"
@@ -46,100 +44,13 @@
             </div>
             <div class="up p-2 d-flex justify-content-between ">
                 <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">كوشه</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">حزاء</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">خزنه</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">ملابس</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">العاب</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">سيارات</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">شنطه</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">شنطه</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">شنطه</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">شنطه</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">شنطه</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">شنطه</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">شنطه</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">شنطه</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">شنطه</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">شنطه</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
+                    @foreach ($products as $product)
+                        <div class="info-box p-3">
+                            {{ $product->id }}
+                            <span class="d-block fs-5">{{ $product->name }}</span>
+                            <span class="fs-5">الكميه المتاحه: {{ $product->quantity }}</span>
+                        </div>
+                    @endforeach
                 </div>
             </div>
         </div>
@@ -167,102 +78,17 @@
                 </div>
             </div>
             <div class="up p-2 d-flex justify-content-between overflow-auto">
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">زهره</span>
-                        <span class="fs-5">1500 ج.م</span>
+                @foreach ($rents as $rent)
+                    <div class="box">
+                        <div class="info-box p-3">
+                            {{ $rent->id }}
+                            <dib class="d-block fs-5">{{ $rent->name }}</dib>
+                            <div class="fs-5">السعر: {{ $rent->sale_price }}</div>
+                            <div class="fs-5">الكميه المتاحه: {{ $rent->quantity }}</div>
+                        </div>
                     </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">مأكولات</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">العاب</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">تيشيرتات</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">كتب</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">موتوسيكلات</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">شنطه</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">شنطه</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">شنطه</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">شنطه</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">شنطه</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">شنطه</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">شنطه</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">شنطه</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">شنطه</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="info-box p-3">
-                        <span class="d-block fs-5">شنطه</span>
-                        <span class="fs-5">1500 ج.م</span>
-                    </div>
-                </div>
+                @endforeach
+
             </div>
         </div>
     </div>
@@ -276,8 +102,7 @@
                 <span>اجمالي السعر</span>
                 <span>النوع</span>
                 <span>الحاله</span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 25"
-                    fill="none">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 25" fill="none">
                     <path fill-rule="evenodd" clip-rule="evenodd"
                         d="M10.325 4.42784C10.751 2.67184 13.249 2.67184 13.675 4.42784C13.8046 4.9629 14.182 5.40386 14.6907 5.61459C15.1993 5.82531 15.7779 5.78044 16.248 5.49384C17.791 4.55384 19.558 6.31984 18.618 7.86384C18.3318 8.33369 18.2871 8.91192 18.4975 9.42022C18.708 9.92852 19.1484 10.3058 19.683 10.4358C21.439 10.8618 21.439 13.3598 19.683 13.7858C19.1479 13.9155 18.707 14.2929 18.4963 14.8015C18.2855 15.3101 18.3304 15.8888 18.617 16.3588C19.557 17.9018 17.791 19.6688 16.247 18.7288C15.7771 18.4427 15.1989 18.3979 14.6906 18.6084C14.1823 18.8188 13.805 19.2593 13.675 19.7938C13.249 21.5498 10.751 21.5498 10.325 19.7938C10.1954 19.2588 9.81797 18.8178 9.30935 18.6071C8.80073 18.3964 8.22206 18.4412 7.752 18.7278C6.209 19.6678 4.442 17.9018 5.382 16.3578C5.66819 15.888 5.71295 15.3098 5.50247 14.8015C5.292 14.2932 4.85157 13.9158 4.317 13.7858C2.561 13.3598 2.561 10.8618 4.317 10.4358C4.85206 10.3062 5.29302 9.92881 5.50375 9.42019C5.71447 8.91157 5.6696 8.3329 5.383 7.86284C4.443 6.31984 6.209 4.55284 7.753 5.49284C8.753 6.10084 10.049 5.56284 10.325 4.42784Z"
                         stroke="#4B465C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
@@ -285,8 +110,8 @@
                         d="M10.325 4.42784C10.751 2.67184 13.249 2.67184 13.675 4.42784C13.8046 4.9629 14.182 5.40386 14.6907 5.61459C15.1993 5.82531 15.7779 5.78044 16.248 5.49384C17.791 4.55384 19.558 6.31984 18.618 7.86384C18.3318 8.33369 18.2871 8.91192 18.4975 9.42022C18.708 9.92852 19.1484 10.3058 19.683 10.4358C21.439 10.8618 21.439 13.3598 19.683 13.7858C19.1479 13.9155 18.707 14.2929 18.4963 14.8015C18.2855 15.3101 18.3304 15.8888 18.617 16.3588C19.557 17.9018 17.791 19.6688 16.247 18.7288C15.7771 18.4427 15.1989 18.3979 14.6906 18.6084C14.1823 18.8188 13.805 19.2593 13.675 19.7938C13.249 21.5498 10.751 21.5498 10.325 19.7938C10.1954 19.2588 9.81797 18.8178 9.30935 18.6071C8.80073 18.3964 8.22206 18.4412 7.752 18.7278C6.209 19.6678 4.442 17.9018 5.382 16.3578C5.66819 15.888 5.71295 15.3098 5.50247 14.8015C5.292 14.2932 4.85157 13.9158 4.317 13.7858C2.561 13.3598 2.561 10.8618 4.317 10.4358C4.85206 10.3062 5.29302 9.92881 5.50375 9.42019C5.71447 8.91157 5.6696 8.3329 5.383 7.86284C4.443 6.31984 6.209 4.55284 7.753 5.49284C8.753 6.10084 10.049 5.56284 10.325 4.42784Z"
                         stroke="white" stroke-opacity="0.2" stroke-width="1.5" stroke-linecap="round"
                         stroke-linejoin="round" />
-                    <circle cx="12" cy="12.1108" r="3" stroke="#4B465C" stroke-width="1.5"
-                        stroke-linecap="round" stroke-linejoin="round" />
+                    <circle cx="12" cy="12.1108" r="3" stroke="#4B465C" stroke-width="1.5" stroke-linecap="round"
+                        stroke-linejoin="round" />
                     <circle cx="12" cy="12.1108" r="3" stroke="white" stroke-opacity="0.2" stroke-width="1.5"
                         stroke-linecap="round" stroke-linejoin="round" />
                 </svg>
