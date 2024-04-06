afed83a4560aa60c9d707c27660056b1a35986e4
khalidYousrygit <k@gmail.com>
2024-04-06 06:01:00 +0200

    warehouse export pdf | excel | print

--- resources/views/pages/warehouse/index.blade.php
+++ resources/views/pages/warehouse/index.blade.php
@@ -4,205 +4,7 @@
 
 @section('styles')
     <link rel="stylesheet" href="{{ asset('Assets/Css files/warehouse.css') }}">
-    {{-- First One Style --}}
-    <style>
-        .select-btn,
-        li {
-            display: flex;
-            align-items: center;
-            cursor: pointer;
-        }
 
-        .select-btn {
-            padding: 10px 20px;
-            gap: 10px;
-            border-radius: 5px;
-            justify-content: space-between;
-            border: 1px solid #eee;
-            min-width: 160px;
-        }
-
-        .select-btn i {
-            font-size: 14px;
-            transition: transform 0.3s linear;
-        }
-
-        .wrapper.active .select-btn i {
-            transform: rotate(-180deg);
-        }
-
-        .content {
-            display: none;
-            padding: 20px;
-            margin-top: 15px;
-            background: #fff;
-            border-radius: 5px;
-            border: 1px solid #eee;
-            position: fixed;
-        }
-
-        .wrapper.active .content {
-            display: block;
-        }
-
-        .content .search {
-            position: relative;
-        }
-
-        .search input {
-            height: 50px;
-            width: 100%;
-            outline: none;
-            font-size: 17px;
-            border-radius: 5px;
-            padding: 0 20px 0 43px;
-            border: 1px solid #B3B3B3;
-        }
-
-        .search input:focus {
-            padding-left: 42px;
-            border: 2px solid #4285f4;
-        }
-
-        .search input::placeholder {
-            color: #bfbfbf;
-        }
-
-        .content .options {
-            margin-top: 10px;
-            max-height: 250px;
-            overflow-y: auto;
-            padding-right: 7px;
-        }
-
-        .options::-webkit-scrollbar {
-            width: 7px;
-        }
-
-        .options::-webkit-scrollbar-track {
-            background: #f1f1f1;
-            border-radius: 25px;
-        }
-
-        .options::-webkit-scrollbar-thumb {
-            background: #ccc;
-            border-radius: 25px;
-        }
-
-        .options::-webkit-scrollbar-thumb:hover {
-            background: #b3b3b3;
-        }
-
-        .options li {
-            padding: 5px 13px;
-        }
-
-        .options li:hover,
-        li.selected {
-            border-radius: 5px;
-            background: #f2f2f2;
-        }
-    </style>
-    {{-- Second One Style --}}
-
-    <style>
-        .select-btn,
-        li {
-            display: flex;
-            align-items: center;
-            cursor: pointer;
-        }
-
-        .select-btn-add {
-            padding: 10px 20px;
-            gap: 10px;
-            border-radius: 5px;
-            justify-content: space-between;
-            border: 1px solid #eee;
-            min-width: 160px;
-        }
-
-        .select-btn i {
-            font-size: 14px;
-            transition: transform 0.3s linear;
-        }
-
-        .dropdown.active .select-btn i {
-            transform: rotate(-180deg);
-        }
-
-        .content {
-            display: none;
-            padding: 20px;
-            margin-top: 15px;
-            background: #fff;
-            border-radius: 5px;
-            border: 1px solid #eee;
-            position: fixed;
-        }
-
-        .dropdown.active .content {
-            display: block;
-        }
-
-        .content .search {
-            position: relative;
-        }
-
-        .search input {
-            height: 50px;
-            width: 100%;
-            outline: none;
-            font-size: 17px;
-            border-radius: 5px;
-            padding: 0 20px 0 43px;
-            border: 1px solid #B3B3B3;
-        }
-
-        .search input:focus {
-            padding-left: 42px;
-            border: 2px solid #4285f4;
-        }
-
-        .search input::placeholder {
-            color: #bfbfbf;
-        }
-
-        .content .options {
-            margin-top: 10px;
-            max-height: 250px;
-            overflow-y: auto;
-            padding-right: 7px;
-        }
-
-        .options::-webkit-scrollbar {
-            width: 7px;
-        }
-
-        .options::-webkit-scrollbar-track {
-            background: #f1f1f1;
-            border-radius: 25px;
-        }
-
-        .options::-webkit-scrollbar-thumb {
-            background: #ccc;
-            border-radius: 25px;
-        }
-
-        .options::-webkit-scrollbar-thumb:hover {
-            background: #b3b3b3;
-        }
-
-        .options li {
-            padding: 5px 13px;
-        }
-
-        .options li:hover,
-        li.selected {
-            border-radius: 5px;
-            background: #f2f2f2;
-        }
-    </style>
 
 @endsection
 @section('content')
@@ -242,25 +44,26 @@
     <h2 class="mb-5">المخزن</h2>
 
 
-    <section class="pt-0 rounded-3 position-relative shadow-sm overflow-auto">
+    <div class="component-left me-3 d-flex align-items-center justify-content-end mb-2">
 
-        <div class="component-left me-3 gap-4 d-flex align-items-center">
-            <div class="select-btn select position-relative rounded-3 d-flex align-items-center">
-                <button onclick="dropdown('valueRelease', 'listRelease')">
-                    <span class="fw-bold opacity-50 valueDropdown" id="valueRelease">اصدار</span>
-                    <img src="./Assets/imgs/chevron-down.png" alt="">
-                </button>
-                <div class="options none">
-                    <ul id="listRelease">
-                        <li>PDF</li>
-                        <li>EXCEL</li>
-                        <li>PRINT</li>
-                    </ul>
-                </div>
+        <div class="select-btn border-0 p-0 select position-relative rounded-3 d-flex align-items-center">
+            <button onclick="dropdown('valueRelease', 'listRelease')">
+                <span class="fw-bold opacity-50 valueDropdown" id="valueRelease">اصدار</span>
+                <img src="{{ asset('Assets/imgs/chevron-down.png') }}" alt="">
+            </button>
+            <div class="options none">
+                <ul id="listRelease">
+                    <li id="pdf">PDF</li>
+                    <li id="excel">EXCEL</li>
+                    <li id="print">PRINT</li>
+                </ul>
             </div>
         </div>
+    </div>
+    <section class="pt-0 rounded-3 position-relative shadow-sm overflow-auto">
 
-        <table class="w-100 mb-4 border">
+
+        <table class="w-100 mb-4 border" id="table">
             <thead class="head">
                 <tr>
                     <th>اسم المنتج</th>
@@ -321,6 +124,114 @@
 
 @section('script')
     <script src="{{ asset('Assets/JS files/warehouse.js') }}"></script>
+        {{-- For Exel --}}
+        <script type="text/javascript" src="https://unpkg.com/xlsx@0.15.1/dist/xlsx.full.min.js"></script>
+        {{-- For PDF --}}
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.22/pdfmake.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js">
+        </script>
 
+        {{-- Print and Pdf and Excel  --}}
+        <script>
+            function printTable() {
+                // Clone the table
+                var myTable = document.getElementById("table").cloneNode(true);
+                myTable.setAttribute('border', '1');
+                myTable.setAttribute('cellpadding', '5');
+
+                // Open a new window for printing
+                var printWindow = window.open('');
+                printWindow.document.write('<html dir="rtl"><head><title>Table Contents</title>');
+
+                // Print the Table CSS.
+                // var table_style = document.getElementById("table_style").innerHTML;
+                printWindow.document.write('<style type="text/css">');
+                // printWindow.document.write(table_style);
+                printWindow.document.write('.print-hidden{display:none;}')
+                printWindow.document.write('#table{width:100%;}')
+                printWindow.document.write('</style>');
+                printWindow.document.write('</head>');
+
+                // Print the cloned table
+                printWindow.document.write('<body>');
+                printWindow.document.write('<div id="dvContents">');
+                printWindow.document.write(myTable.outerHTML);
+                printWindow.document.write('</div>');
+                printWindow.document.write('</body>');
+
+                printWindow.document.write('</html>');
+                printWindow.document.close();
+                printWindow.print();
+            }
+            const print = document.getElementById("print");
+            print.addEventListener('click', () => {
+                printTable();
+            });
+            //End Print Table
+
+            // Start PDF File
+            function Export() {
+                var table = document.getElementById('table');
+
+                var lastColumnCells = table.querySelectorAll('td:last-child, th:last-child');
+
+
+                var tableCells = table.querySelectorAll('th, td');
+                tableCells.forEach(function(cell) {
+                    cell.style.textAlign = "right";
+                });
+
+                html2canvas(table, {
+                    onrendered: function(canvas) {
+                        lastColumnCells.forEach(function(cell) {
+                            cell.style.display = '';
+                        });
+
+                        tableCells.forEach(function(cell) {
+                            cell.style.textAlign = "";
+                        });
+
+                        var data = canvas.toDataURL();
+
+                        var docDefinition = {
+                            content: [{
+                                image: data,
+                                width: 500
+                            }]
+                        };
+                        pdfMake.createPdf(docDefinition).download("Table.pdf");
+                    }
+                });
+            }
+
+
+            const pdfBtn = document.getElementById("pdf");
+            pdfBtn.addEventListener('click', () => {
+                Export()
+            });
+            // End PDF File
+
+            // Start Exel Sheet
+            function htmlTableToExcel(type) {
+                var data = document.getElementById('table');
+
+
+                var excelFile = XLSX.utils.table_to_book(data, {
+                    sheet: "sheet1"
+                });
+                XLSX.write(excelFile, {
+                    bookType: type,
+                    bookSST: true,
+                    type: 'base64'
+                });
+                XLSX.writeFile(excelFile, 'ExportedFile:HTMLTableToExcel.' + type);
+            }
+
+            const exelBtn = document.getElementById("excel");
+            exelBtn.addEventListener('click', () => {
+                htmlTableToExcel('xlsx')
+            });
+            // End Exel Sheet
+        </script>
 
 @endsection
